7985cc3737d06ab3f3cca2e80eaa39fc67363eb7
Zachariasz Soja <zachary3003@gmail.com>
2022-07-19 01:06:44 +0200

    Added database context

---
+++ Spotify.Database/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Spotify.Domain.Models;
+
+namespace Spotify.Database
+{
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Song> Songs { get; set; }
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<Playlist> Playlists { get; set; }
+    }
+}
