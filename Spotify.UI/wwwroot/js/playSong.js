30709ee5a4a305bc2dc1815b5f90bc2b19d6889e
Zachariasz Soja <zachary3003@gmail.com>
2022-08-01 01:51:09 +0200

    Songs are played one after another

--- Spotify.UI/wwwroot/js/playSong.js
+++ Spotify.UI/wwwroot/js/playSong.js
@@ -1,11 +1,28 @@
-﻿function playSong(fileName, id) {
+﻿function playSong(index) {
+    let audio = document.getElementById('audio');
+    let song = document.getElementById(`song-play-${index}`);
+
+    let doesSongExist = song !== null;
+
+    if (!doesSongExist) {
+        song = document.getElementById('song-play-1')
+    }
+
+    let src = song.dataset.filename;
+    let id = song.dataset.id;
+
     updateSongInfo(id);
 
-    let audio = document.getElementById('audio');
-    audio.src = `/Songs/${fileName}`;
-    audio.play();
+    audio.src = `/Songs/${src}`;
 
-    axios.post('/AddPlay/' + id).catch(error => console.log(error));
+    audio.onended = () => {
+        playSong(++index);
+    }
+
+    if (doesSongExist) {
+        audio.play();
+        axios.post('/AddPlay/' + id).catch(error => console.log(error));
+    }
 }
 
 function updateSongInfo(id) {
